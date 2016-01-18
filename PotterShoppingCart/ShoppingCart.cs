b56a28833659265145ea5d82b513bbe0256e7768
Monkey.B <thisisbinbin@gmail.com>
2016-01-19 00:04:03 +0800

    修改Product code通過所有測試案例

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -8,38 +8,61 @@ namespace PotterShoppingCart
 {
     public class ShoppingCart
     {
-        private List<Product> _products = new List<Product>();
-
+        private List<Product> _products = new List<Product>();        
+        
         public double GetTotalPrice()
         {
             double totalPrice = 0;
-            if (_products.Count == 1)
+            List<List<Product>> producsGroup = GetProductsGroups();
+
+            foreach (var group in producsGroup)
             {
-                totalPrice = _products.Select(x => x.Price * 1).Sum();
+                totalPrice += group.Select(o => o.Price * GetDiscount(group.Count())).Sum();
             }
-            else if (_products.Count == 2 && _products.Select(o => o.ProductId).Distinct().Count() == 2)
-            {
-                totalPrice = _products.Select(x => x.Price * 0.95).Sum();
-            }
-            else if (_products.Count == 3 && _products.Select(o => o.ProductId).Distinct().Count() == 3)
-            {
-                totalPrice = _products.Select(x => x.Price * 0.9).Sum();
-            }
-            else if (_products.Count == 4)
-            {
-                if (_products.Select(o => o.ProductId).Distinct().Count() == 4)
-                    totalPrice = _products.Select(x => x.Price * 0.8).Sum();
-                else if (_products.Select(o => o.ProductId).Distinct().Count() == 3)
-                    totalPrice = _products.Take(3).Select(o => o.Price * 0.9).Sum() + _products.Take(1).Select(o => o.Price * 1).Sum();
-            }
-            else if (_products.Count == 5)
-            {
-                if (_products.Select(o => o.ProductId).Distinct().Count() == 5)
-                    totalPrice = _products.Select(x => x.Price * 0.75).Sum();
-                else if (_products.Select(o => o.ProductId).Distinct().Count() == 3)
-                    totalPrice = _products.Take(3).Select(o => o.Price * 0.9).Sum() + _products.Take(2).Select(o => o.Price * 0.95).Sum();
-            }
+
             return totalPrice;
+        }
+
+        private double GetDiscount(int episodeCount)
+        {
+            switch (episodeCount)
+            {
+                case 2:
+                    return 0.95;
+                case 3:
+                    return 0.9;
+                case 4:
+                    return 0.8;
+                case 5:
+                    return 0.75;
+                case 1:
+                default:
+                    return 1;
+            }
+        }
+
+        private List<List<Product>> GetProductsGroups()
+        {
+            List<List<Product>> producsGroup = new List<List<Product>>();
+            var groups = _products.GroupBy(p => p.ProductId);
+
+            for (int i = 0; i < groups.Max(g => g.Count()); i++)
+            {
+                producsGroup.Add(new List<Product>());
+            }
+
+            foreach (var product in _products.OrderBy(o => o.ProductId))
+            {
+                foreach (var group in producsGroup)
+                {
+                    if (!group.Any(o => o.ProductId == product.ProductId))
+                    {
+                        group.Add(product);
+                        break;
+                    }
+                }
+            }
+            return producsGroup;
         }
 
         public void AddProducts(List<Product> products)
