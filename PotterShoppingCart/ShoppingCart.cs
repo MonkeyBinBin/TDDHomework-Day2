c5b25473968cdd22ced103041c0dfadeba7c1391
Monkey.B <thisisbinbin@gmail.com>
2016-01-19 22:33:33 +0800

    變數名稱修正,改用Dictionary取得折扣

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -8,14 +8,15 @@ namespace PotterShoppingCart
 {
     public class ShoppingCart
     {
-        private List<Product> _products = new List<Product>();        
-        
+        private List<Product> _products = new List<Product>();
+        private readonly Dictionary<int, double> _discount = new Dictionary<int, double>() { { 2, 0.95 }, { 3, 0.9 }, { 4, 0.8 }, { 5, 0.75 }, { 1, 1 } };
+
         public double GetTotalPrice()
         {
             double totalPrice = 0;
-            List<List<Product>> producsGroup = GetProductsGroups();
+            List<List<Product>> productsGroup = GetProductsGroups();
 
-            foreach (var group in producsGroup)
+            foreach (var group in productsGroup)
             {
                 totalPrice += group.Select(o => o.Price * GetDiscount(group.Count())).Sum();
             }
@@ -25,35 +26,24 @@ namespace PotterShoppingCart
 
         private double GetDiscount(int episodeCount)
         {
-            switch (episodeCount)
-            {
-                case 2:
-                    return 0.95;
-                case 3:
-                    return 0.9;
-                case 4:
-                    return 0.8;
-                case 5:
-                    return 0.75;
-                case 1:
-                default:
-                    return 1;
-            }
+            double discount = 1;
+            _discount.TryGetValue(episodeCount, out discount);
+            return discount;
         }
 
         private List<List<Product>> GetProductsGroups()
         {
-            List<List<Product>> producsGroup = new List<List<Product>>();
+            List<List<Product>> productsGroup = new List<List<Product>>();
             var groups = _products.GroupBy(p => p.ProductId);
 
             for (int i = 0; i < groups.Max(g => g.Count()); i++)
             {
-                producsGroup.Add(new List<Product>());
+                productsGroup.Add(new List<Product>());
             }
 
             foreach (var product in _products.OrderBy(o => o.ProductId))
             {
-                foreach (var group in producsGroup)
+                foreach (var group in productsGroup)
                 {
                     if (!group.Any(o => o.ProductId == product.ProductId))
                     {
@@ -62,7 +52,7 @@ namespace PotterShoppingCart
                     }
                 }
             }
-            return producsGroup;
+            return productsGroup;
         }
 
         public void AddProducts(List<Product> products)
